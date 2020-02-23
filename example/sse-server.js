3c8d8a1add9a1774bedde2b857a2dc7ef33a4c1b
Alex Frankish <alexfrankish@gmail.com>
2020-02-23 17:35:20 +0800

    init

---
+++ example/sse-server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const SseStream = require('ssestream')
+
+const app = express()
+app.use(serveStatic(__dirname))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.get('/sse', (req, res) => {
+  console.log('new connection')
+
+  const sseStream = new SseStream(req)
+  sseStream.pipe(res)
+  const pusher = setInterval(() => {
+    sseStream.write({
+      event: 'server-time',
+      data: new Date().toTimeString()
+    })
+  }, 7000)
+
+  res.on('close', () => {
+    console.log('lost connection')
+    clearInterval(pusher)
+    sseStream.unpipe(res)
+  })
+})
+
+app.listen(9000, (err) => {
+  if (err) throw err
+  console.log('server ready on http://localhost:9000')
+})
